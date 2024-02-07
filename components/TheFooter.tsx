be77b53f18f72f5849d835928dad8a47fc06c4a9
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2024-02-07 23:21:31 +0000

    fix(Footer): add footer max-width size

--- components/TheFooter.tsx
+++ components/TheFooter.tsx
@@ -1,13 +1,13 @@
 export default function TheFooter() {
   return (
-    <footer className="text-xs">
-      <div className="px-4 pt-4 pb-8 border-t border-neutral-100 flex flex-col gap-2 items-center justify-between sm:flex-row sm:max-w-2xl sm:mx-auto md:pt-8 md:pb-16">
+    <footer className="mt-14">
+      <div className="flex items-center justify-between sm:max-w-7xl sm:mx-auto pt-4 pb-8 border-t border-neutral-100 flex-col gap-2 sm:flex-row md:pt-8 md:pb-16">
         <span>&copy; Galsen UI. Tous droits réservés</span>
         <ul className="inline-flex items-center gap-2">
           {/* TODO: add correct links */}
-          <li>Facebook</li>
-          <li>Twitter</li>
-          <li>Github</li>
+          <li className="text-sm">Facebook</li>
+          <li className="text-sm">Twitter</li>
+          <li className="text-sm">Github</li>
         </ul>
       </div>
     </footer>
